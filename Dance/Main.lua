30003a569c3f5d4b61963830b7791c1e593a1890
Hapalarast <88484210+Hapalarast@users.noreply.github.com>
2021-09-07 17:26:29 +0300

    Main.lua

--- Dance/Main.lua
+++ Dance/Main.lua
@@ -21,49 +21,8 @@ local spinnerImage = workspace:addChild(GUI.image(1, 1, {1, 1}))
 
 ------------------------------------------------------------------------------------------
 
-local function changeColor(hue, saturation)
-	for i = 1, #spinners do
-		for y = 1, image.getHeight(spinners[i]) do
-			for x = 1, image.getWidth(spinners[i]) do
-				local background, foreground, alpha, symbol = image.get(spinners[i], x, y)
-				local hBackground, sBackground, bBackground = color.integerToHSB(background)
-				local hForeground, sForeground, bForeground = color.integerToHSB(foreground)
-				image.set(
-					spinners[i],
-					x,
-					y,
-					color.HSBToInteger(hue, saturation, bBackground),
-					color.HSBToInteger(hue, saturation, bForeground),
-					alpha,
-					symbol
-				)
-			end
-		end
-	end
-	spinnerImage.image = spinners[currentSpinner]
-end
 
-workspace.eventHandler = function(workspace, object, e1, e2, e3, e4, e5)
-	if e1 == "key_down" then
-		workspace:stop()
-	elseif e1 == "touch" then
-		spinnerHue = spinnerHue + spinnerHueStep * (e5 == 1 and -1 or 1)
-		if spinnerHue > 360 then
-			spinnerHue = 0
-		elseif spinnerHue < 0 then
-			spinnerHue = 360
-		end
-		changeColor(spinnerHue, 1)
-	end
-	
-	currentSpinner = currentSpinner + 1
-	if currentSpinner > #spinners then
-		currentSpinner = 1
-	end
-	spinnerImage.image = spinners[currentSpinner]
-	
-	workspace:draw()
-end
+
 
 ------------------------------------------------------------------------------------------
 
